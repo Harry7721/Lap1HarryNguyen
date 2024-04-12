f0f8e5d204023c129b2ef8ccb130eb3d2556d663
Harry Nguyen <143207451+Harry7721@users.noreply.github.com>
2024-04-12 17:57:40 -0400

    Initial Lab 1 Commit

---
+++ Assets/Scripts/Forces.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Forces : MonoBehaviour
+{
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+             
+        Vector3 move = new Vector3(hor, 0.0f, ver);
+        GetComponent<Rigidbody>().AddForce(move * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Velocities.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Velocities : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void FixedUpdate ()
+	{
+        float hor = 0;
+        float ver = 0;
+			if (Input.GetKey ("a"))
+				hor = -1;
+			 if (Input.GetKey ("d"))
+				hor = 1;
+			if (Input.GetKey ("w"))
+				ver = 1;
+			 if (Input.GetKey ("s"))
+				ver = -1;
+			Vector3 vel = new Vector3 (hor, 0, ver);
+			GetComponent<Rigidbody>().position+= vel * speed*Time.deltaTime;
+			
+			
+		
+		}
+		
+		}
+
